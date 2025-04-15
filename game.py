993c76aee4e1fea00909aa5186b3ba57c283bcf4
sloth-on-meth <sam@1a38.nl>
2025-04-15 16:25:13 +0200

    fluid

--- game.py
+++ game.py
@@ -9,11 +9,23 @@ BALL_RADIUS = 10
 g = 500  # gravity px/s^2
 friction = 0.99  # velocity damping
 hexagon_radius = 250
-rotation_speed = math.radians(30)  # degrees per second
+base_rotation_speed = math.radians(30)  # degrees per second
 
 # Ball
 ball_pos = [WIDTH // 2, HEIGHT // 2 - 100]
 ball_vel = [100, 0]
+
+# Water area
+WATER_LEVEL = HEIGHT // 2 + 100
+WATER_DRAG = 0.8
+
+# Easter egg counter
+c_press_count = 0
+rainbow_mode = False
+rainbow_hue = 0
+
+# Wind
+wind = 0
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -46,6 +58,7 @@ def reflect_vector(vel, normal):
     ]
 
 angle = 0
+rotation_speed = base_rotation_speed
 running = True
 while running:
     dt = clock.tick(FPS) / 1000
@@ -53,12 +66,38 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_c:
+                c_press_count += 1
+                if c_press_count >= 10:
+                    rainbow_mode = True
+            if event.key == pygame.K_a:
+                rotation_speed = -base_rotation_speed * 1.5
+            if event.key == pygame.K_d:
+                rotation_speed = base_rotation_speed * 1.5
+        if event.type == pygame.KEYUP:
+            if event.key in (pygame.K_a, pygame.K_d):
+                rotation_speed = base_rotation_speed
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        wind = -50
+    elif keys[pygame.K_RIGHT]:
+        wind = 50
+    else:
+        wind = 0
 
     # Update angle
     angle += rotation_speed * dt
 
     # Update velocity and position
+    ball_vel[0] += wind * dt
     ball_vel[1] += g * dt
+
+    if ball_pos[1] > WATER_LEVEL:
+        ball_vel[0] *= WATER_DRAG
+        ball_vel[1] *= WATER_DRAG
+
     ball_vel[0] *= friction
     ball_vel[1] *= friction
     ball_pos[0] += ball_vel[0] * dt
@@ -70,7 +109,6 @@ while running:
         p1 = hexagon[i]
         p2 = hexagon[(i + 1) % 6]
 
-        # Line segment vector and normal
         edge = (p2[0] - p1[0], p2[1] - p1[1])
         edge_len = math.hypot(*edge)
         if edge_len == 0:
@@ -78,24 +116,30 @@ while running:
         edge_unit = (edge[0] / edge_len, edge[1] / edge_len)
         normal = (-edge_unit[1], edge_unit[0])
 
-        # Distance from ball to line
         rel = (ball_pos[0] - p1[0], ball_pos[1] - p1[1])
         dist = rel[0]*normal[0] + rel[1]*normal[1]
 
-        # Project point onto edge
         proj_len = rel[0]*edge_unit[0] + rel[1]*edge_unit[1]
         if -BALL_RADIUS < dist < BALL_RADIUS and 0 <= proj_len <= edge_len:
-            # Move out of wall
             overlap = BALL_RADIUS - dist
             ball_pos[0] += normal[0] * overlap
             ball_pos[1] += normal[1] * overlap
             ball_vel = reflect_vector(ball_vel, normal)
 
     # Draw
-    screen.fill((10, 10, 30))
+    if rainbow_mode:
+        rainbow_hue = (rainbow_hue + 60 * dt) % 360
+        color = pygame.Color(0)
+        color.hsva = (rainbow_hue, 100, 100, 100)
+        screen.fill(color)
+    else:
+        screen.fill((10, 10, 30))
+
     pygame.draw.polygon(screen, (200, 200, 255), hexagon, 2)
+    pygame.draw.rect(screen, (30, 30, 100), (0, WATER_LEVEL, WIDTH, HEIGHT - WATER_LEVEL))
     pygame.draw.circle(screen, (255, 50, 50), (int(ball_pos[0]), int(ball_pos[1])), BALL_RADIUS)
     pygame.display.flip()
 
 pygame.quit()
 sys.exit()
+
